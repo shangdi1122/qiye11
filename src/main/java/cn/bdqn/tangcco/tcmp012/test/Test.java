dbb4558420193299215c831c0911fc54a3cd865b
shangdi1122 <1399070052@qq.com>
2017-08-10 08:51:08 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp012/test/Test.java
@@ -0,0 +1,12 @@
+package cn.bdqn.tangcco.tcmp012.test;
+
+/**
+ * Created by THINKPAD on 2017/8/10.
+ */
+public class Test {
+    @org.junit.Test
+    public void print() {
+        System.out.println("谢谢大家");
+        System.out.println("dagehap");
+    }
+}
